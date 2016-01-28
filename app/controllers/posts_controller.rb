7d53117f3a44a19ceba3cc6cd5680fc32883f3a1
Evrim Persembe <evrimfeyyaz@users.noreply.github.com>
2016-01-28 10:31:12 +0300

    Add viewing an individual post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+
+    @skip_header = true
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ActiveRecord::Base
   belongs_to :category
 
+  def previous_post
+    Post.where("id < ?", id).order("id DESC").first
+  end
+
+  def next_post
+    Post.where("id > ?", id).order("id ASC").first
+  end
+
   # TODO: Implement paperclip.
   def image
     "test.jpg"
