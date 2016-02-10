ffcdf05736d514f5aa4f829010744eb41f899993
Evrim Persembe <evrimfeyyaz@users.noreply.github.com>
2016-02-10 11:04:37 +0300

    Use contact, login, and logout routes for all relevant actions instead of messages, and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   root to: "posts#index"
 
   get  'contact', to: 'messages#new'
+  post 'contact', to: 'messages#create'
   resources :posts, path: '', only: [:index, :show]
   resources :categories, only: [:show]
-  resources :messages, only: [:create]
 
   namespace :admin do
     root to: "posts#index"
-    get 'login', to: 'sessions#new'
-    get 'logout', to: 'sessions#destroy'
-    resource :sessions, only: [:create, :destroy]
+    get     'login', to: 'sessions#new'
+    post    'login', to: 'sessions#create'
+    delete  'logout', to: 'sessions#destroy'
     resources :posts, except: [:show] do
       collection do
         delete :destroy_multiple
